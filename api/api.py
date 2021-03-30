e62370fc3e6dd248b16edf8d0c70158c94e97d82
saraems <sara.ems.94@gmail.com>
2021-03-30 21:47:51 +0200

    ATSI-18 Serve app with static_url_path

--- api/api.py
+++ api/api.py
@@ -1,8 +1,12 @@
 from datetime import datetime
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../build', static_url_path='/')
 
 @app.route('/api/time')
 def get_current_time():
     return {'time': {'hour': datetime.now().hour, 'minute': datetime.now().minute}}
+
+@app.errorhandler(404)
+def not_found(e):
+    return app.send_static_file('index.html')
