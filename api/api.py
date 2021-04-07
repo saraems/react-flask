a8c36b675a80eb84c4ce14f9fa3acdc1ea81e248
saraems <sara.ems.94@gmail.com>
2021-04-07 14:34:38 +0200

    ASTI-42 Change the path

--- api/api.py
+++ api/api.py
@@ -7,8 +7,12 @@ app = Flask(__name__, static_folder='../build', static_url_path='/')
 def get_current_time():
     return {'time': {'hour': datetime.now().hour, 'minute': datetime.now().minute}}
 
-@app.errorhandler(404)
-def not_found(e):
+# @app.errorhandler(404)
+# def not_found(e):
+#     return app.send_static_file('index.html')
+
+@app.route('/')
+def index():
     return app.send_static_file('index.html')
 
 if __name__ == "__main__":
